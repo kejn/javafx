5f34f0efad0646a0c2fc08a85a224f100a966005
Kamil Niemczyk <kamil.niemczyk@capgemini.com>
2015-09-02 12:58:43 +0200

    Solved passing national characters to POST (Adding book).

--- smallibrary/smallibrary/src/main/java/com/capgemini/starterkit/javafx/smallibrary/dataprovider/impl/DataProviderImpl.java
+++ smallibrary/smallibrary/src/main/java/com/capgemini/starterkit/javafx/smallibrary/dataprovider/impl/DataProviderImpl.java
@@ -2,6 +2,7 @@ package com.capgemini.starterkit.javafx.smallibrary.dataprovider.impl;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -11,6 +12,7 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.apache.http.entity.StringEntity;
 import org.apache.log4j.Logger;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -126,14 +128,17 @@ public class DataProviderImpl implements DataProvider {
 			connection = (HttpURLConnection) url.openConnection();
 			connection.setRequestMethod("POST");
 			connection.setRequestProperty("Content-Type", "application/json");
+			connection.setRequestProperty("Content-Length", Integer.toString(bookJSON.toString().length()));
 			connection.setDefaultUseCaches(false);
 			connection.setDoOutput(true);
 
 			DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
-			wr.writeBytes(bookJSON.toString());
+			StringEntity bookTo = new StringEntity(bookJSON.toString(), "UTF-8");
+			bookTo.writeTo(wr);
 			wr.close();
 
-			BufferedReader rd = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
+			InputStream inputStream = connection.getInputStream();
+			BufferedReader rd = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
 			String line;
 			while ((line = rd.readLine()) != null) {
 				response.append(line);
